edd9155feac6a37ef46f585ec04d9c6c7b32aa9e
Milad Fadavvi <Fadavvi@users.noreply.github.com>
2020-08-18 15:09:45 +0430

    cleanup!

--- SQLCipher-CE-CentOS7.sh
+++ SQLCipher-CE-CentOS7.sh
@@ -1,22 +1,16 @@
-#!/usr/bin/env bash
-
-export SQLITE_HAS_CODEC
+export SQLITE_HAS_CODEC > /dev/null
 export SQLITE_TEMP_STORE=2
-
-
-yum install openssl-devel tcl-devel readline-devel git -y >> /dev/null
-
-git clone https://github.com/sqlcipher/sqlcipher.git >> /dev/null
-
-cd sqlcipher
-./configure --enable-tempstore=yes CFLAGS="-DSQLITE_HAS_CODEC" LDFLAGS="-lcrypto" >> /dev/null
-
-make >> /dev/null
-
-make install >> /dev/null
-
+cd /tmp/
+echo -e '* Install OpenSSL/TCL/ReadLine and Git\n'
+yum install openssl-devel tcl-devel readline-devel git -y > /dev/null
+echo -e '* Clone SqlCipher Repo\n'
+git clone --quiet https://github.com/sqlcipher/sqlcipher.git > /dev/null
+cd /tmp/sqlcipher/
+echo -e '* Config & Compile\n'
+./configure --enable-tempstore=yes CFLAGS="-DSQLITE_HAS_CODEC" LDFLAGS="-lcrypto" > /dev/null
+make > /dev/null
+make install > /dev/null
 cp sqlcipher /usr/local/sbin/
-
+echo -e 'SQLCipher Info:\n'
 sqlcipher --version
-
 exit 0
